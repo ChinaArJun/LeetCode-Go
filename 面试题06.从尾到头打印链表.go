6bc9231f9521f9b52fdb437f3e5c8664fa214e24
ArJun <iloveyou138@vip.qq.com>
2020-03-31 12:42:30 +0800

    update

--- 面试题06.从尾到头打印链表.go
+++ 面试题06.从尾到头打印链表.go
@@ -1,5 +1,6 @@
 package cn
-//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。 
+
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
 //
 // 
 //
@@ -24,7 +25,27 @@ package cn
  *     Next *ListNode
  * }
  */
+/*type ListNode struct {
+	Val int
+	Next *ListNode
+}*/
 func reversePrint(head *ListNode) []int {
-
+	// 解题思路：取值，反转
+	// 老是报错 ： panic: runtime error: invalid memory address or nil pointer dereference
+	//			[signal SIGSEGV: segmentation violation code=0x1 addr=0x8 pc=0x4bb046]
+	//
+	if head.Next == nil {
+		return nil
+	}
+	var arr []int
+	for head != nil {
+		arr = append(arr, head.Val)
+		head = head.Next
+	}
+	res := make([]int, len(arr))
+	for i := 0; i < len(arr); i ++ {
+		res[i] = arr[len(arr)- 1 - i]
+	}
+	return res
 }
 //leetcode submit region end(Prohibit modification and deletion)
