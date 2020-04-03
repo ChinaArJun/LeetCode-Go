1dc26454023dcca48193ec4231007fc0eb9e12c5
ArJun <iloveyou138@vip.qq.com>
2020-04-03 12:03:05 +0800

    update

---
+++ 面试题24.反转链表_test.go
@@ -0,0 +1,30 @@
+package cn
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	list := ListNode{Val:1, Next: &ListNode{Val:2, Next: &ListNode{Val: 3, Next: &ListNode{Val:4, Next:nil}}}}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+
+		{"",args{head:&list}, nil},
+		{"",args{head:&list}, nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
