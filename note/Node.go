5f72aaaaa92209b7fea011b3a6053a6434d322ba
llx <luolianxi@zhongzhuang.com>
2020-07-21 17:18:20 +0800

    up

---
+++ note/Node.go
@@ -0,0 +1,6 @@
+package note
+
+type ListNode struct {
+   Val int
+   Next *ListNode
+}
---
+++ note/面试题06.从尾到头打印链表.go
@@ -0,0 +1,58 @@
+package note
+
+import (
+	"fmt"
+)
+
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1] 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 10000 
+//
+
+  
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+/*type ListNode struct {
+	Val int
+	Next *ListNode
+}*/
+func reversePrint(head *cn.ListNode) []int {
+	// 解题思路：取值，反转
+	// 老是报错 ： panic: runtime error: invalid memory address or nil pointer dereference
+	//			[signal SIGSEGV: segmentation violation code=0x1 addr=0x8 pc=0x4bb046]
+	/*
+	之前if head.Next == nil 老是报错，是因为head.Next存在null的现象
+	*/
+	if head == nil {
+		return nil
+	}
+	var arr []int
+	for head != nil {
+		arr = append(arr, head.Val)
+		head = head.Next
+	}
+	fmt.Println(arr)
+	res := make([]int, len(arr))
+	for i := 0; i < len(arr); i ++ {
+		res[i] = arr[len(arr)- 1 - i]
+	}
+	return res
+}
+//leetcode submit region end(Prohibit modification and deletion)
